e632e7dbf8d4c6e9030ac350de9c7f9bb925ee18
Hafsa Salman <hafsalman0521@gmail.com>
2026-03-03 18:11:25 +0500

    Example Cars

---
+++ CarSales/data/initialCars.js
@@ -0,0 +1,18 @@
+export const initialCars = [
+  {
+    id: "1",
+    name: "Civic",
+    company: "Honda",
+    price: "5000000",
+    modelYear: "2022",
+    status: "Available",
+  },
+  {
+    id: "2",
+    name: "Corolla",
+    company: "Toyota",
+    price: "4500000",
+    modelYear: "2021",
+    status: "Sold",
+  },
+];
